e66a594311e6c59dcb85582b8af3c5dcf3877237
vagrant <vagrant@vagrant.vm>
2020-11-20 08:43:18 +0000

    taskbox UI

---
+++ src/components/TaskList.stories.js
@@ -0,0 +1,63 @@
+// src/components/TaskList.stories.js
+
+import React from 'react';
+
+// Changed TaskList to PureTaskList
+// The curly brackets matter! Or else bifga3 error cannot find store
+import { PureTaskList } from './TaskList';
+import * as TaskStories from './Task.stories';
+
+// Import redux data
+//import store from '../lib/redux';
+
+export default {
+  component: PureTaskList,
+  title: 'TaskList',
+  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
+};
+
+const Template = args => <PureTaskList {...args} />;
+
+// Redux Data
+//export const StoreData = Template.bind({});
+//StoreData.args = {
+//  tasks: store.getState().tasks,
+//};
+
+export const Default = Template.bind({});
+Default.args = {
+  // Shaping the stories through args composition.
+  // The data was inherited from the Default story in task.stories.js.
+  tasks: [
+    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+    { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+    { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+    { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+    { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+  ],
+};
+
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Default story.
+  tasks: [
+    ...Default.args.tasks.slice(0, 5),
+    { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+  ],
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  tasks: [],
+  loading: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Loading story.
+  ...Loading.args,
+  loading: false,
+};
